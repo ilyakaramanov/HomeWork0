310b9f50a021a1e5931b7395f05860e5b2eb8d6a
ilyakaramanov <ilya.karamanov@gmail.com>
2018-12-21 16:39:10 +0200

    initial commit

--- src/HomeWork0.java
+++ src/HomeWork0.java
@@ -1,6 +1,10 @@
 public class HomeWork0 {
 
     public static void main(String[] args) {
+
+        /*
+        Запуск всех методов
+        */
         task1();
         task2();
         task4();
